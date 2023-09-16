612496aaea0b879f992892c37ea2d838da4a7843
pedro-valdez <pedro.a.valdez00+dev@gmail.com>
2023-09-16 16:32:43 -0500

    Input is now controleld

--- app/Search/Searchbar/Input.tsx
+++ app/Search/Searchbar/Input.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
+
 export default function SearchbarInput() {
+	const [searchText, setSearchText] = useState("")
+
 	return (
 		<input
 			type="search"
 			placeholder="Search..."
 			className="input bg-transparent w-full focus:outline-0"
+			value={searchText}
+			onChange={e => {
+				setSearchText(e.currentTarget.value)
+			}}
 		/>
 	)
 }
