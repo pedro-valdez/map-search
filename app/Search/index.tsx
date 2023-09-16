accfbdc04d1ae02830e06dfcfce830ab2ae551ee
pedro-valdez <pedro.a.valdez00+dev@gmail.com>
2023-09-16 18:09:45 -0500

    Removed unecessary space

--- app/Search/index.tsx
+++ app/Search/index.tsx
@@ -5,7 +5,7 @@ import Searchbar from "./Searchbar"
 
 export default function Search() {
 	return (
-		<div className="group fixed z-20 left-4 top-4 w-[calc(100%-2rem)]  sm:max-w-sm">
+		<div className="group fixed z-20 left-4 top-4 w-[calc(100%-2rem)] sm:max-w-sm">
 			<Searchbar />
 			<Results />
 		</div>
