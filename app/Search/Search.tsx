e7188743e3254f23687e7baede6b2541110de90f
pedro-valdez <pedro.a.valdez00+dev@gmail.com>
2023-09-18 13:10:11 -0500

    Results overflow scroll only when needed

--- app/Search/Search.tsx
+++ app/Search/Search.tsx
@@ -16,7 +16,7 @@ export default function Search() {
 	return (
 		<form
 			id="search"
-			className="group fixed z-20 left-4 top-4 w-[calc(100%-2rem)] sm:max-w-sm max-h-[calc(100vh-2rem)] overflow-y-scroll"
+			className="group fixed z-20 left-4 top-4 w-[calc(100%-2rem)] sm:max-w-sm max-h-[calc(100vh-2rem)] overflow-y-auto"
 			onSubmit={e => {
 				e.preventDefault()
 				const shouldOpenModal = searchResults.length > 0
